bfdce96708407b1f58947ca32c3f9c126906fbee
joe-vh <joevanhien@gmail.com>
2017-01-12 18:17:08 +0100

    Create accumulator.js

---
+++ accumulator.js
@@ -0,0 +1,59 @@
+const shape = [[{
+  a: 1,
+  b: 2,
+  c: [
+    1,
+    2,
+    {
+      a: 1,
+      b: 2,
+      c: [
+        1,
+        2,
+        {
+          a: 1
+        }
+      ]
+    },
+    [
+      1,
+      2,
+      {
+        a: 1,
+        b: 2,
+        c: [
+          1,
+          2,
+          {
+            a: 1
+          }
+        ]
+      }
+    ]
+  ],
+  d: {
+    a: 1,
+    b: 4,
+    c: 5
+  }
+}]];
+
+// recursive reduction/accumulation
+
+const typeSwitcher = (item) => {
+  if (typeof item === 'number') {
+    return item;
+  }
+
+  if (typeof item === 'object') {
+    return iterate(item);
+  }
+}
+
+const iterate = (shape) => {
+  return Object.keys(shape).reduce((acc, item) => {
+    return acc + typeSwitcher(shape[item]);
+  }, 0);
+}
+
+console.log(iterate(shape));
